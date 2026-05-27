3f7dd59825d9d172df1b1d04375c4875c17e89e2
enock80180-cyber <enock80180@icloud.com>
2026-05-27 03:50:07 +0200

    Create hours_tracker.py

---
+++ hours_tracker.py
@@ -0,0 +1,8 @@
+savings = 350
+
+if savings > 499:
+    print("Buy the ticket to the netherlands!")
+elif savings > 199:
+    print("keep saving,you are so close!")
+else:
+    print("Time to grind harder.")
