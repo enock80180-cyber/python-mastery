ff64f818381defa3feb2ae127ddc4f6599eb4efd
enock80180-cyber <enock80180@icloud.com>
2026-05-28 08:48:42 +0200

    Create Traineeship_skills.py

---
+++ Traineeship_skills.py
@@ -0,0 +1,10 @@
+# Note 1: Creatinbg a list (our train of skills) using square brackets[]
+my_skills = ["Variables", "If-statements", "Comments", "Lists"]
+
+print("Reviewing my portfolio skills.....")
+
+# Note 2: Checking if "Lists" is inside our skills list
+if "Lists" in my_skills:
+     print("Success! You are ready to talk about Lists in your interview.")
+else:
+     print("keep praticing,you will get there soon!")
