4013c1646bb97bf9fe4d69c41d849fa075484192
enock80180-cyber <enock80180@icloud.com>
2026-05-28 07:27:50 +0200

    Create Traineeship_goal.py

---
+++ Traineeship_goal.py
@@ -0,0 +1,15 @@
+# Note 1: Write why you praticing today
+my_why = "Solve real and Tangible problems"
+
+# Note 2: Write a reminder about your IND sticker timeline 
+months_left = 5
+
+# Note 3: Write what your hobbies are
+my_hobbies = "Reading and solving puzzles"
+
+print("My Driving Purpose: " + my_why)
+print("Months until my IND sticker: " + str(months_left))
+print("lesuire time: " + my_hobbies)
+
+# My ultimat goal sentence
+print("In "+ str(months_left)+ " months i will " + my_why + " in IT job!")
