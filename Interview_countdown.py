16cc05f4f09753c517ebfb98d968edb151ecb5d8
enock80180-cyber <enock80180@icloud.com>
2026-05-29 09:48:31 +0200

    Create Interview_countdown.py

---
+++ Interview_countdown.py
@@ -0,0 +1,8 @@
+# Note 1: A list of traineeships you are targeting
+target_company = ["Calco" , "Mottop" , "Young capital"]
+
+print("Starting my taineeship application broadcast....")
+
+# Note 2: The loop Machine (The robot visit each company one by one)
+for company in target_company:
+     print("Hello " + company + ", I am ready to solve real problems for you!")
