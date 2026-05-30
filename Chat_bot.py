15875e5bc200727c196b591087961a9773fe5ef7
enock80180-cyber <enock80180@icloud.com>
2026-05-30 22:00:55 +0200

    Create Chat_bot.py

---
+++ Chat_bot.py
@@ -0,0 +1,9 @@
+print("Booting up your personal IT Assistant....")
+
+# Shortcut: We put your name and target right here in the code 
+user_name = "Future IT Pro"
+target = "Calco"
+
+# The robot uses them perfectly!
+print("Nice to meet you, " + user_name + "!")
+print("Awesome choice! " + user_name + " + " + target + "is going to be a perfect match! 🚀")
