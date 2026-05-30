05e6f53c530792dcd95f75f8c71b9debd15dcb4f
enock80180-cyber <enock80180@icloud.com>
2026-05-30 14:07:51 +0200

    Create interview_pratice.py

---
+++ interview_pratice.py
@@ -0,0 +1,12 @@
+# NOTE 1: Starting with 3 days left to pratice
+days_left = 9
+
+print("Starting my intensive interview prep week......")
+
+# NOTE 2: The "While" Machine (keeps running AS LONG AS days_left is bigger than 0)
+while days_left > 0:
+    print(str(days_left) + " days left! Practice coding on my ipad....")
+    # NOTE 3: Subtracting 1 day each time the loop runs     
+    days_left = days_left - 1
+
+    print("0 days left! The pratice is over. I am offical ready for the interview! 🚀")
