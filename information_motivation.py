23937e5bf661fc91520c61c560f27a14cadbb2a8
enock80180-cyber <enock80180@icloud.com>
2026-05-30 17:05:19 +0200

    Create information_motivation.py

---
+++ information_motivation.py
@@ -0,0 +1,13 @@
+# Note 1: We start with a reminder to stay motivated
+keep_going = True
+
+print("Turning on the motivation machine...")
+
+# Note 2: The Loop runs as long as keep_going is equal to True
+while keep_going == True:
+    print("Keep pushing! Your IT traineeship is getting closer! 🚀")
+    
+    # Note 3: The Emergency Stop
+    keep_going = False 
+
+print("Machine stopped safely.")
