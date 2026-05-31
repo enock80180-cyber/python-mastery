2949bba09c0aeb8e38cf40d54e7882f8a53384b7
enock80180-cyber <enock80180@icloud.com>
2026-05-31 19:16:12 +0200

    Create Budget_bouncer.py

---
+++ Budget_bouncer.py
@@ -0,0 +1,11 @@
+# 1.  Your current savings account balance 
+my_savings = 1800
+
+# 2.  The cost of the traineeship laptop 
+laptop_cost = 1200
+
+# 3.  The Bouncer checks the budget  (Note: No qoutes here!)
+if my_savings >= laptop_cost:
+    print("Access Granted!  Buy the Laptop 💻")
+else:
+    print("Access Denied! keep saving! 🐷")
