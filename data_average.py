33a3b81e2a5fc217b9bde2dca7b8ef6165f4e846
enock80180-cyber <enock80180@icloud.com>
2026-05-31 23:35:28 +0200

    Create data_average.py

---
+++ data_average.py
@@ -0,0 +1,14 @@
+#  1. Alist of the trainee assessment score (out of 100)
+scores = [85, 90, 78, 92,88]
+
+# 2. Get the total sum of all scores 
+total_score = sum(scores)
+
+# 3. Count how many scores are in the list (this willequal 5)
+number_of_scores = len(scores)
+
+# 4. Calculate the average by dividing (use the / slash symbol) 
+average_score = total_score / number_of_scores
+
+# 5. Use combo-print to show the final data insight
+print("The trainee's average score is" ,average_score)
