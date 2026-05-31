d92075ee7efd33222703152464c09aa1eba1c18c
enock80180-cyber <enock80180@icloud.com>
2026-05-31 20:06:41 +0200

    Create data_picker.py

---
+++ data_picker.py
@@ -0,0 +1,12 @@
+# 1. A List of monthly salaries ( data rows)
+salaries = [2200, 2500, 2700, 3100, 3500]
+
+# 2. Grab the first salary using position. 0
+first_salary = salaries[0]
+
+# 3. Grab the last salary using posistion -1
+last_salary = salaries[-1]
+
+# 4. Print them out
+print("The starting trainee salary is:", first_salary)
+print("The senior salary at the end is:",last_salary)
