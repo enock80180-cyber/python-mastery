c5a368f933ae16987508b32af1202ef99b7e3803
enock80180-cyber <enock80180@icloud.com>
2026-05-31 22:52:59 +0200

    Create data_sum.py

---
+++ data_sum.py
@@ -0,0 +1,8 @@
+#  1. A list. of company project budgets
+budgets = [5000, 1200, 3500, 8000, 15000]
+
+# 2. Use the magic sum tool to add them all up instantly
+total_spend = sum(budgets)
+
+# 3. Use your combo-printto show the results together!
+print("Total company expenditure:", total_spend)
