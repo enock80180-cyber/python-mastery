974ba798c5bc0126e9a5c167bae4ec46bf1019a0
enock80180-cyber <enock80180@icloud.com>
2026-06-02 21:32:21 +0200

    Create data_min.py

---
+++ data_min.py
@@ -0,0 +1,8 @@
+# 1. A list of server downtime minutes per day
+downtime_minutes = [14, 25, 3, 42, 8, 19]
+
+# 2. Use the magic min tool to find the lowest number instantly 
+lowest_downtime = min(downtime_minutes)
+
+# 3. Use your combo-print to display the best perfomance day
+print("The lowest server downtime recorded was" , lowest_downtime)
