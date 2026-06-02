82efb9f7e638024cedb4f86a86905b8b2d02f582
enock80180-cyber <enock80180@icloud.com>
2026-06-02 19:44:02 +0200

    Create data_max.py

---
+++ data_max.py
@@ -0,0 +1,8 @@
+# 1. A list of applicant assessment scores.
+applicant_scores = [67, 89, 195, 75, 91, 82]
+
+# 2. Use the magic max tool to find the highest number instantly 
+highest_scores = max(applicant_scores)
+
+# 3. Use your combo-print to display the peak perfomance 
+print("The highest score is:",highest_scores)
