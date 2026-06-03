ebcec7a9dd9d3d04ba66239b57707e3d3f37eda3
enock80180-cyber <enock80180@icloud.com>
2026-06-03 18:18:44 +0200

    Create data_sorter.py

---
+++ data_sorter.py
@@ -0,0 +1,9 @@
+# 1. A messy list of system response times (in milliseconds)
+respond_times = [120, 45, 310, 85, 190]
+
+# 2. Use the magic sorted tool to organise from the lowest to the highest 
+ordered_times = sorted(respond_times)
+
+# 3. Printt out both lists to see the magic transformation!
+print("The messy original data", respond_times)
+print("perfectlt sorted data", ordered_times)
