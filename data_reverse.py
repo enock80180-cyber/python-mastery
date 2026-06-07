0d327aecd661698b2bfd840e0802a38397fb7017
enock80180-cyber <enock80180@icloud.com>
2026-06-08 00:34:16 +0200

    Create data_reverse.py

---
+++ data_reverse.py
@@ -0,0 +1,9 @@
+# 1. Server logs arranged from the oldest to newest
+server_logs = ["Log_01_AM", "Log_02_AM", "Log_04_PM"]
+print("Oldest to the Newest:", server_logs)
+
+# 2. Use .reverse() to flip the list structure in place
+server_logs.reverse()
+
+# 3. Print the results to verify the new order
+print("Newest to Oldest:", server_logs)
