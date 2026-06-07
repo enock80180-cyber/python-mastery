14f1b68a7c623b6df8019cd6a3936aa7b3c4e9c6
enock80180-cyber <enock80180@icloud.com>
2026-06-08 00:59:08 +0200

    Create data_sort.py

---
+++ data_sort.py
@@ -0,0 +1,9 @@
+# 1. A randomized list of transaction amounts(in dollars)
+sales_transactions = [120, 45, 990, 15, 350]
+print("Randomized Sales:", sales_transactions)
+
+# 2. Use .sort() to organize the list from the lowest to the highest
+sales_transactions.sort()
+
+# 3. Print the sorted list to confirm the organization
+print("Sorted Sales (low. to high):", sales_transactions)
