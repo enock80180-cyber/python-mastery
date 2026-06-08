143e46702577047d6ad9e9b510618bbe9e0bbb01
enock80180-cyber <enock80180@icloud.com>
2026-06-08 12:09:59 +0200

    Create data_in.py

---
+++ data_in.py
@@ -0,0 +1,13 @@
+# 1. The official VIP quest list
+vip_list = ["Alice", "Bob", "Charlie", "David"]
+
+# 2. Check if "Charlie is on the the of the VIP( This should be True)"
+is_charlie_invited = "Charlie" in vip_list
+
+# 3. Check if "Malet" in the Vip_list( This should be False)
+is_malet_invited = "Malet" in vip_list
+
+# 4. Print out the True/False results
+print("==== CALCO VIP SECURITY CHECK ====")
+print("Is charlie on the list?:", is_charlie_invited)
+print("is Malet on the list?:", is_malet_invited)
