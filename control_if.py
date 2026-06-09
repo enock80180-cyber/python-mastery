8f5db7f77b72f3a0947fe730acbfb73e159ecd21
enock80180-cyber <enock80180@icloud.com>
2026-06-09 10:22:27 +0200

    Create control_if.py

---
+++ control_if.py
@@ -0,0 +1,12 @@
+# 1. The cost of the parking and the driver's current balance 
+parking_cost = 10
+user_balance = 45
+
+print( "Driver approaches the gate......" )
+
+# 2. The Decision Gate ( The "if" statement ) 
+# This code ONLY triggers if the math statement is True!
+if user_balance >= parking_cost:
+    print("---- GATE OPEN: Drive on through! ----")
+
+print ( "System scan complete." )
