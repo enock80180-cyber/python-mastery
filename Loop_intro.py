52089e86935e517d7582ca70c8c741e9befb8f68
enock80180-cyber <enock80180@icloud.com>
2026-06-11 11:00:47 +0200

    Create Loop_intro.py

---
+++ Loop_intro.py
@@ -0,0 +1,7 @@
+# Project #43: The Automatic Box scanner (Your First Loop)
+
+# range(1, 6) tells Python to start counting at 1 and stop BEFORE 6 (so 1, 2, 3, 4, 5)
+for box in range(1, 6):
+     print(f"[SCANNER] Box #{box} successfulley scanned into the system. ")
+
+print("--- ALL BOXES SCANNED: Job done!---")
