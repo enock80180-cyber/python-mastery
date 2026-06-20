d39ba2acd3ed002254193dd39a083e15c64def01
enock80180-cyber <enock80180@icloud.com>
2026-06-21 01:51:19 +0200

    Create loop_function.py

---
+++ loop_function.py
@@ -0,0 +1,15 @@
+# Project #52: The Reusable Scanner Function
+
+# 1. Define the recipe (saves it for later)
+def scan_box():
+    print("📦 Scanning...")
+    print("⚖️ Weight logged.")
+
+# 2. Execute the shift timeline
+print("✅ Starting shift")
+scan_box()
+
+print("☕️ Taking a break")
+scan_box()
+
+print("🛑 ending shift")
