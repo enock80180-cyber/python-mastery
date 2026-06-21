a4b90a30445ad1210a96365443665e7ca8632f05
enock80180-cyber <enock80180@icloud.com>
2026-06-22 00:46:12 +0200

    Create robot_handshake.py

---
+++ robot_handshake.py
@@ -0,0 +1,17 @@
+Project #55: The Robot Handshake (Functions with Parameters)
+
+# 1. Define the handshake recipe with an 'animal' parameter
+def activate_robot(animal):
+    print("👋🏻 High-five!")
+    print("🤖 Beep boop!")
+    print(f"🔈 Sound effect: {animal}!")
+    print("----")
+
+print("✅ Robot System Initialized\n")
+
+# 2. Call the function with different Data inputs 
+activate_robot("dog")
+activate_robot("cat")
+activate_robot("lion")
+
+print("🛑 Robot System Powered Down")
