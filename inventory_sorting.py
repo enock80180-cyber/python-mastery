b7ce0e55d932b5edc428060f617c4aac67b01256
enock80180-cyber <enock80180@icloud.com>
2026-07-05 17:20:23 +0200

    Create inventory_sorting.py

---
+++ inventory_sorting.py
@@ -0,0 +1,16 @@
+# Project #67: Inventory Sorting System
+
+# 1. Setup a messy,unorganized inventory
+inventory = ["🧪 Potion", "⚔️Sword", "🍏 Apple", "🛡️ sheild"]
+
+print("🎒 Messy Backpack:")
+print(inventory)
+
+# 2. Use .sort() to.  automatically arrange the items alphabetically
+inventory.sort()
+
+print("\n⚡️ 'Auto-Sort' Button Pressed!")
+
+# 3.Print the newly organized bag 
+print("🎒 Organized Backpack (A-Z):")
+print(inventory)
