8bee50429c70e587b662037b0b28eb12341e9802
enock80180-cyber <enock80180@icloud.com>
2026-07-06 01:19:41 +0200

    Create inventory_combine.py

---
+++ inventory_combine.py
@@ -0,0 +1,20 @@
+# Project #71: Inventory Combination System 
+
+# 1. Setup the player's backpack
+backpack = ["⚔️ Steel Sword", "🛡️ Sheild"]
+print("🎒 Main Backpack:")
+print(backpack)
+
+# 2. Setup the items found inside a treasure chest
+treasure_chest = ["💰 Gold Coins", "🧪 Health Potion","🔑 Room Key"]
+print("\n📦 Treasure Chest Opened! Inside you see:")
+print(treasure_chest)
+
+print("\n⚡️ Looting the chest.... Adding all items to your backpack!")
+
+# 3. Combine both lists together using the + OPERATOR
+backpack = backpack + treasure_chest
+
+# 4. Print the newly expanded. bcakpack 
+print("\n🎒 Updated Backpack with all the loot:")
+print(backpack)
