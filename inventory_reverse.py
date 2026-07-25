b170d825d52c031db7798ff5fd6ab172b8a443f4
enock80180-cyber <enock80180@icloud.com>
2026-07-25 18:32:20 +0200

    Create inventory_reverse.py

---
+++ inventory_reverse.py
@@ -0,0 +1,16 @@
+# Project #74: Inventory Reversing System
+
+# 1. Setup a list of itemsadded over time (oldest to newest)
+loot_history = ["🪵 Stick", "⚔️ Sword", "🗡️ Dagger","Steel Sword", "👑 Golden Crown"]
+
+print("📜 Quest History (Oldest -> Newest):")
+print(loot_history)
+
+# 2.Reverse the list order
+loot_history.reverse()
+
+print("\n⚡️ Reversing list order....")
+
+# 3.Print the reversed history(newest to oldest)
+print("📜 Recent Achievements (Newest -> Oldest):")
+print(loot_history)
